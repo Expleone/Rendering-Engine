8ca1cfcca0b116384affec46fa857c3559b188fe
Ihor Holub <holubihor@gmail.com>
2026-03-22 17:20:14 +0100

    setup

--- main.cpp
+++ main.cpp
@@ -1,3 +1,77 @@
-//
-// Created by expleoene on 3/22/26.
-//
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+// Функція для обробки помилок GLFW
+void glfw_error_callback(int error, const char* description) {
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+void settings_window(float color[3]) {
+    ImGui::Begin("Налаштування");
+    ImGui::Text("Це вікно працює у CLion!");
+    ImGui::ColorEdit3("Колір фону", color);
+    ImGui::End();
+}
+
+int main() {
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) return 1;
+
+    const char* glsl_version = "#version 330";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "CLion ImGui Project", nullptr, nullptr);
+    if (!window) return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Turn on V-Sync
+
+    // Init Dear ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+
+    const char* font_path = "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf";
+    ImGui::GetIO().Fonts->AddFontFromFileTTF(font_path, 18.0f, nullptr, io.Fonts->GetGlyphRangesCyrillic());
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start of new frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Interface window
+        static float color[3] = { 0.1f, 0.1f, 0.1f };
+        settings_window(color);
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(color[0], color[1], color[2], 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    // Clean up
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
